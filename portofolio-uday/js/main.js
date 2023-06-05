e5db5ca63d8d0d02f8e60993caa321586eba4679
Uday-Singh G <91188742+Uday-Singh1@users.noreply.github.com>
2023-06-05 21:01:57 +0200

    added-functions

--- portofolio-uday/js/main.js
+++ portofolio-uday/js/main.js
@@ -1,23 +1,54 @@
+// class Search {
+//   constructor() {
+//     this.searchInput = document.getElementById("search-input");
+//     this.searchForm = document.querySelector(".search-form");
+//     this.searchForm.addEventListener("submit", this.handleSearch.bind(this));
+//   }
+
+//   handleSearch(event) {
+//     event.preventDefault();
+//     const searchText = this.searchInput.value.toLowerCase();
+
+//     const projects = document.querySelectorAll(".project");
+//     projects.forEach((project) => {
+//       const projectTitle = project.querySelector("h3").textContent.toLowerCase();
+//       if (projectTitle.includes(searchText)) {
+//         project.style.display = "block";
+//       } else {
+//         project.style.display = "none";
+//       }
+//     });
+//   }
+// }
+
 class Search {
   constructor() {
-    this.searchInput = document.getElementById("search-input");
-    this.searchForm = document.querySelector(".search-form");
-    this.searchForm.addEventListener("submit", this.handleSearch.bind(this));
+    this.searchForm = document.querySelector('.search-form');
+    this.searchInput = document.querySelector('#search-input');
+    this.searchForm.addEventListener('submit', this.search.bind(this));
   }
 
-  handleSearch(event) {
+  search(event) {
     event.preventDefault();
-    const searchText = this.searchInput.value.toLowerCase();
+    const searchTerm = this.searchInput.value.toLowerCase();
 
-    const projects = document.querySelectorAll(".project");
-    projects.forEach((project) => {
-      const projectTitle = project.querySelector("h3").textContent.toLowerCase();
-      if (projectTitle.includes(searchText)) {
-        project.style.display = "block";
-      } else {
-        project.style.display = "none";
-      }
-    });
+    switch (searchTerm) {
+      case 'project':
+      case 'projecten':
+        window.location.href = '/projecten.html';
+        break;
+      case 'cv':
+      case 'CV':
+        window.location.href = './img/Uday-Singh-CV.png';
+        break;
+      case 'contact':
+        case 'contacten':
+        window.location.href = '/contact.html';
+        break;
+      default:
+        window.location.reload();
+        break;
+    }
   }
 }
 
@@ -29,6 +60,11 @@ function scrollToProjects() {
   projectsElement.scrollIntoView({ behavior: "smooth", block: "start" });
 }
 
+function scrollToContact() {
+  const contactElement = document.querySelector(".contact__form");
+  contactElement.scrollIntoView({ behavior: "smooth", block: "start" });
+}
+
 
 function toggleFlip(card) {
   card.classList.toggle('hover');
@@ -36,52 +72,67 @@ function toggleFlip(card) {
 
 
 class TypedAnimation {
-    constructor(elementId, texts) {
-      this.textElement = document.getElementById(elementId);
-      this.texts = texts;
-      this.index = 0;
-      this.isFinished = false;
-    }
-  
-    typeText(text) {
-      const length = text.length;
-      let i = 0;
-  
-      const typingInterval = setInterval(() => {
-        if (i === length) {
-          clearInterval(typingInterval);
-          if (this.index === this.texts.length - 1) {
-            this.textElement.textContent = text;
-            this.isFinished = true;
-          } else {
-            setTimeout(() => this.typeNextText(), 950);
-          }
-        }
-  
-        this.textElement.textContent += text.charAt(i);
-        i++;
-      }, 90); 
-    }
-  
-    typeNextText() {
-      this.index++;
-      this.textElement.textContent = '';
-      this.typeText(this.texts[this.index]);
-    }
-  
-    startAnimation() {
-      this.typeText(this.texts[this.index]);
-    }
+  constructor(elementId, texts) {
+    this.textElement = document.getElementById(elementId);
+    this.texts = texts;
+    this.index = 0;
+    this.isFinished = false;
+    this.onCompleteCallback = null;
   }
-  
-  // Gebruik van de klasse
-  const typedAnimation = new TypedAnimation('text', [
-    "Hallo ik ben Uday,",
-    "Ik ben een Ambitieuze Software Developer."
-  ]);
-  typedAnimation.startAnimation();
-  
 
+  typeText(text, callback) {
+    const length = text.length;
+    let i = 0;
+
+    const typingInterval = setInterval(() => {
+      if (i === length) {
+        clearInterval(typingInterval);
+        callback();
+      }
+
+      this.textElement.textContent += text.charAt(i);
+      i++;
+    }, 90);
+  }
+
+  typeNextText() {
+    if (this.index === this.texts.length) {
+      this.isFinished = true;
+      if (this.onCompleteCallback) {
+        this.onCompleteCallback();
+      }
+      return;
+    }
+
+    const text = this.texts[this.index];
+    this.textElement.textContent = '';
+    this.typeText(text, () => {
+      setTimeout(() => {
+        this.index++;
+        this.typeNextText();
+      }, 300);
+    });
+  }
+
+  startAnimation() {
+    this.typeNextText();
+  }
+
+  onAnimationComplete(callback) {
+    this.onCompleteCallback = callback;
+  }
+}
+
+const typedAnimation = new TypedAnimation('text', [
+  "Hallo ik ben Uday,",
+  "Ik ben een ambitieuze softwareontwikkelaar.",
+  "Mijn droom is om mijn eigen techbedrijf te starten."
+]);
+
+typedAnimation.startAnimation();
+
+
+  
 //cursor
 // const cursor = document
 //     .querySelector(".custom-cursor");
